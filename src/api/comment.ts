2007e5701420854074da8efe6cbe7af1bb310ae9
GiuntaLucas <lucas-giunta@outlook.com>
2022-10-18 21:30:09 +0200

    search ssr

--- src/api/comment.ts
+++ src/api/comment.ts
@@ -1,3 +1,8 @@
-export const getComment = (postId: number) => {
+import { Comment } from "../interfaces/comment";
+export const getComment = (postId: number): Promise<Comment[]> => {
     return fetch(`https://jsonplaceholder.typicode.com/posts/${postId}/comments`).then(x => x.json());
+}
+
+export const getComments = (): Promise<Comment[]> => {
+    return fetch(`https://jsonplaceholder.typicode.com/comments`).then(x => x.json());
 }
--- src/api/post.ts
+++ src/api/post.ts
@@ -3,3 +3,7 @@ import { Post } from "../interfaces/post";
 export const getPosts = (): Promise<Post[]> => {
     return fetch('https://jsonplaceholder.typicode.com/posts').then(x => x.json());
 }
+
+export const getPostsByUser = (userId: number): Promise<Post[]> => {
+    return fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`).then(x => x.json());
+}

04d3954bf853eed2d40847639cb1c73b3e3a260d
GiuntaLucas <lucas-giunta@outlook.com>
2022-10-18 18:06:57 +0200

    issue ids

--- src/components/PostLink.svelte
+++ src/components/PostLink.svelte
@@ -3,7 +3,8 @@
   import { getUser } from "../api/user";
   import { getComment } from '../api/comment';
 
-  export let id;
+  export let postId;
+  export let userId;
   let user;
   let comments;
   onMount(async () => {
@@ -12,8 +13,8 @@
   });
 </script>
 
-<a class="text-orange-600" href="/user/{id}">
+<a class="text-orange-600" href="/user/{userId}">
   {user ? user.name : ''}
 </a>
 |
-<a href="/post/{id}/comments">Comments: ({comments ? comments.length : 0}) </a>
+<a href="/post/{postId}/comments">Comments: ({comments ? comments.length : 0}) </a>
